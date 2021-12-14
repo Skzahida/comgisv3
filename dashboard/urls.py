6e76c906ed1ff60e6a8119c2fe5b99ff529a59d6
Himanshi <himanshigoyal5@gmail.com>
2021-12-14 14:29:39 +0530

    removed imports of wagtail

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,11 +4,6 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path ,include
 
-from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.core import urls as wagtail_urls
-from wagtail.documents import urls as wagtaildocs_urls
-
-
 from . import views
 
 # from rest_framework import routers
