8bf0b5142d7e2852ad16a0506791ded37162e6ec
monikashah <monikapatira@gmail.com>
2022-12-02 22:46:47 +0530

    Upated watergis

--- dashboard/models.py
+++ dashboard/models.py
@@ -18,7 +18,34 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django import forms
 from crispy_forms.helper import FormHelper
 
-
+class UploadWellPictureModel(models.Model):
+    picture = models.ImageField( upload_to='WellPics/', blank=True, null=True, default='WellPics/noImage.jpg')
+    name = models.CharField(max_length=100, blank=True, null=True)
+    well_nm = models.CharField(max_length=100, blank=True, null=True)
+    radius = models.IntegerField(blank=True, null=True)
+    depth = models.IntegerField(blank=True, null=True)
+    level = models.IntegerField(blank=True, null=True)
+    village = models.CharField(max_length=100, blank=True, null=True)
+    district = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    pincode = models.CharField(max_length=8, blank=True, null=True)
+    lat = models.CharField(max_length=15)
+    lng = models.CharField(max_length=15)
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.picture = self.compressImage(self.picture)
+        super(UploadWellPictureModel, self).save(*args, **kwargs)
+    def compressImage(self,picture):
+        imageTemproary = Image.open(picture)
+        imageTemproary = imageTemproary.convert('RGB')
+        outputIoStream = BytesIO()
+        imageTemproary = imageTemproary.resize( (1020,573) ) 
+        imageTemproary.save(outputIoStream , format='JPEG', quality=60)
+        outputIoStream.seek(0)
+        picture = InMemoryUploadedFile(outputIoStream,'ImageField', "%s.jpg" % picture.name.split('.')[0], 'image/jpeg', sys.getsizeof(outputIoStream), None)
+        return picture
+    class Meta:
+       managed = True
 # class CustomAccountManager(BaseUserManager):
 #     def create_superuser(self, username, password, **other_fields):
 #         default_ward = MumbaiWardBoundary2Jan2022.objects.get(id=1)
